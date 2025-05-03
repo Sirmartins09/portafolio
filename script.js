40ceb90101853e7c6edcd08e06f37eb789f7ed49
Martin Dean <martindn6@gmail.com>
2025-05-03 19:17:29 -0300

    utima actulización

--- script.js
+++ script.js
@@ -21,16 +21,18 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[0].classList.add("htmlcss");
+        habilidades[1].classList.add("photoshop");
+        habilidades[2].classList.add("dataentry");
+        habilidades[3].classList.add("logistica-interna");
+        habilidades[4].classList.add("microsoft-excel");
+        habilidades[5].classList.add("microsoft-word");
+        habilidades[6].classList.add("comunicacion");
+        habilidades[7].classList.add("trabajo");
+        habilidades[8].classList.add("datos-documentacion"); // <--- FALTABA ESTA
+        habilidades[9].classList.add("dedicacion");
+        habilidades[10].classList.add("autonomia-proactividad");
+        habilidades[11].classList.add("organizacion-trabajo");
     }
 }
 
